9dcb7068e3e0965641c1230b34f455d2451168fc
nativ <nativatir@gmail.com>
2021-02-01 09:22:35 +0200

    add all files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <x-button></x-button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import XButton from './components/XButton'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    XButton
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+// import * as components from './components'
+
+// const ComponentLibrary = {
+//   install(Vue) {
+//     // components
+//     for (const componentName in components) {
+//       const component = components[componentName]
+
+//       Vue.component(component.name, component)
+//     }
+//   }
+// }
+
+// export default ComponentLibrary
+
+// if (typeof window !== 'undefined' && window.Vue) {
+//   window.Vue.use(ComponentLibrary)
+// }
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
